5774c0b88aed82edbc1b2e5a5946c6eaeee31bb0
MonaMahmoud <monamohamedfahmy@gmail.com>
2022-05-01 09:42:59 +1000

    final adjustments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,18 +28,13 @@ export const putDb = async(content) => {
 
 
 
-    console.info('after line 18');
     const tx = jateDb.transaction('jate', 'readwrite');
-    //console.info('after line 20');
 
     const store = tx.objectStore('jate');
-    // console.info('after line 24');
 
     const request = store.put({ id: 1, jate: content });
-    ////console.info('after line 26');
 
     const result = await request;
-    //console.info('after line 29');
 
     console.log('🚀 - data saved to the database', result);
 };
